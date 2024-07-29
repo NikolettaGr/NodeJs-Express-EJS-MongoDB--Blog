b9e3ad95780935271c96cc5d72a4edc96cd83020
NikolettaGr <grishaberN@outlook.com>
2024-07-29 14:54:21 +0200

    Setup app.js and create a main.js

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const expresslayout = require('express-ejs-layouts');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+
+// Templating Engine
+app.use(expresslayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+
+app.use('/'.require('./server/routes/main'));
+
+app.listen(PORT, () => {
+    console.log('App listening on port ${PORT}');
+});
